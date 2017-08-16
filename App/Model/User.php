91878b498d4f3704389687713cac8ffa868ca290
Chris Cornutt <enygma@phpdeveloper.org>
2017-08-16 02:25:52 -0500

    adding the "keys" relationship to the User model

--- App/Model/User.php
+++ App/Model/User.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
 
-class User
+class User extends Model
 {
     const STATUS_ACTIVE = 'active';
     const STATUS_DISABLED = 'disabled';
@@ -10,4 +11,9 @@ class User
     protected $fillable = [
         'username', 'name', 'email', 'password', 'password_reset_date', 'status'
     ];
+
+    public function keys()
+    {
+        return $this->hasMany('\App\Model\ApiKey');
+    }
 }
