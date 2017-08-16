8a5249c9384b1d10d802d3fb1cd62d4fdbd7751c
Chris Cornutt <enygma@phpdeveloper.org>
2017-08-16 02:26:20 -0500

    adding the login and "/test" route for testing

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,3 +1,10 @@
 <?php
 
 $app->get('/', '\App\Controller\IndexController:index');
+
+$app->group('/user', function() use ($app) {
+    $app->post('/login', '\App\Controller\UserController:login');
+});
+
+$app->post('/test', '\App\Controller\IndexController:test')
+    ->add(new \App\Middleware\SessionValidate());
